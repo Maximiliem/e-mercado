3d7f34b55a7ebcf20ccbc8b88582ebcb1e1b92ee
Maximiliem <maximiliem@yahoo.com>
2023-08-11 15:54:08 -0300

    agregado de container con getelements by class name

--- js/products.js
+++ js/products.js
@@ -1,7 +1,8 @@
 const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 
-// const container = document.getElementById
+const container = document.getElementsByClassName("container");
 /*
 Para esto me parece que hay que apoyarse en el ejercicio 7.2.1.Fetch
 porque tiene pinta de ser muy similar
  */
+
