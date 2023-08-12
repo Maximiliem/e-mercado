65674d91779fe2cb4665cd9aba1bb561e39e6f88
Maximiliem <maximiliem@yahoo.com>
2023-08-12 19:06:03 -0300

    comentario en products.js

--- js/products.js
+++ js/products.js
@@ -1,6 +1,6 @@
 const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 
-const container = document.getElementsByClassName("container");
+const container = document.getElementsByClassName("container"); //ver el tema de un querySelector
 /*
 Para esto me parece que hay que apoyarse en el ejercicio 7.2.1.Fetch
 porque tiene pinta de ser muy similar
