7c0a6ba18b530dc5302315ff7a6f469ba8d211d0
Maximiliem <maximiliem@yahoo.com>
2023-08-14 21:43:58 -0300

    CSS added into products page

--- js/products.js
+++ js/products.js
@@ -1,13 +1,13 @@
 // Esperar hasta que el contenido del DOM (estructura HTML) esté completamente cargado
 document.addEventListener("DOMContentLoaded", () => {
-   
+
   // Obtener el primer elemento con la clase "lista-cars" que fue añadida en products.html
   const listaCars = document.getElementsByClassName("lista-cars")[0];
 
   // Realizar una solicitud a la URL proporcionada usando el método fetch
   fetch("https://japceibal.github.io/emercado-api/cats_products/101.json")
 
-  // Cuando la respuesta de la solicitud se recibe, la convertimos a formato JSON
+    // Cuando la respuesta de la solicitud se recibe, la convertimos a formato JSON
     .then((response) => response.json())
 
     // Después de convertir la respuesta a JSON, trabajamos con los datos obtenidos
@@ -15,22 +15,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // Inicializar una variable para almacenar el HTML que vamos a generar
       let listaHtml = "";
-      
+
       // Iterar a través de los elementos en el arreglo "products" dentro de los datos
       data.products.forEach((item) => {
 
         // Mostrar en la consola la información del producto actual
         console.log(item);
 
-         // Generar el fragmento de HTML para mostrar la información del producto
-        listaHtml += `<div>
-              <img class="imagenCars" src=${item.image}>
-              Nombre: ${item.name}
-              Descripcion: ${item.description}
-              USD: ${item.currency}
-              Precio: ${item.cost}
-              Vendidos: ${item.soldCount}
-            </div>
+        // Generar el fragmento de HTML para mostrar la información del producto
+        listaHtml += `<div class="producto">
+         <img class="imagenCars" src=${item.image}>
+         
+         <div class="divTexto">
+
+         <div class="divNombre">
+         <p class="nombre">${item.name}</p>
+         </div>
+
+         <div class="divDescripcion">
+         <p class="descripcion">${item.description}</p>
+         </div>
+
+         <div class="divPrecio">
+         <p class="precio">Precio: ${item.currency} ${item.cost}</p>
+         </div>
+
+         <div class="divVendidos">
+         <p class="vendidos">Vendidos: ${item.soldCount}</p>
+         </div>
+         </div>
+       </div>
           `;
       });
 
