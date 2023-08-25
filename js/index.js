a63a8fd2082f45ff589a442382bd9563b4bab0b4
sambuchito <139599455+sambuchito@users.noreply.github.com>
2023-08-25 18:47:37 -0300

    inicio cambios search bar

--- js/index.js
+++ js/index.js
@@ -12,3 +12,36 @@ document.addEventListener("DOMContentLoaded", function(){
         window.location = "products.html"
     });
 });
+
+
+
+const searchInput = document.getElementById('search-input');
+const searchResults = document.getElementById('search-results');
+
+searchInput.addEventListener('input', function() {
+  const searchText = searchInput.value.toLowerCase();
+
+  searchResults.innerHTML = '';
+
+  Promise.all([loadData('data1.json'), loadData('data2.json')])
+    .then(results => {
+      const allData = results.flat();
+
+      const matchingResults = allData.filter(item => item.nombre.toLowerCase().includes(searchText));
+
+      matchingResults.forEach(result => {
+        const resultItem = document.createElement('div');
+        resultItem.textContent = result.nombre;
+        searchResults.appendChild(resultItem);
+      });
+    });
+});
+
+function loadData(jsonFile) {
+  return fetch(jsonFile)
+    .then(response => response.json())
+    .catch(error => {
+      console.error('Error al cargar el archivo JSON:', error);
+      return [];
+    });
+}
