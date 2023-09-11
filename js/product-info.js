77238318fec14e32c52563998eb036b8f2b9b832
CoreDiLeone <fireejuegos@gmail.com>
2023-09-11 20:54:37 -0300

    Consigna 1 y consigna 2 listas, faltan los estilos para la info de los productos

--- js/product-info.js
+++ js/product-info.js
@@ -1,103 +1,27 @@
-/* 
-CONSIGNA 2
-recuperar los id del localstorage anterior en products.js y hacer fetch al json de product-info en función a cada id de cada producto 
-*/
-// document.addEventListener("DOMContentLoaded", function (){
-const recuperarId = localStorage.getItem("productoSeleccionado");
-const urlRecuperacion = `https://japceibal.github.io/emercado-api/products/${recuperarId}.json`;
-const infoEnContenedor = document.getElementsByClassName('container');
-// mostrarEnContenedor();
+/*ENTREGA Nª3 / CONSIGNA-2*/
 
-// function mostrarEnContenedor(){
-//     fetch(urlRecuperacion)
-//     .then((response) => response.json())
-//     .then((data) => {
-//         console.log("sufrimiento 1");
-//       mostrarInner(data.collection.items);
-//     })
-//     .catch((error) => {
-//       console.error("Error al realizar la petición:", error);
-//       console.log("sufrimiento 2");
-//     });
-// };
-
-//     function mostrarInner(items) {
-//         infoEnContenedor.innerHTML = "";
-
-//         const nombre = item.links[0].name;
-//         const descripcion = item.data[0].description;
-//         const costo = item.data[0].cost;
-//         const moneda = item.data[0].currency;
-//         const cantidadVendidos = item.data[0].soldCount;
-//         // aca van las imagenes
-
-//         const itemsObtenidos = document.createElement("div");
-//         itemsObtenidos.classList.add("items-resultantes");
-//         itemsObtenidos.innerHTML = `
-//         <h3>${nombre}</h3>
-//         <p>${descripcion}</p>
-//         <p>${moneda} Precio: ${costo}</p>
-//         <p>Cantidad de vendidos: ${cantidadVendidos}</p>
-//         <img src="${imageUrl}" alt="${title}">
-//         `;
-
-//         infoEnContenedor.appendChild(itemsObtenidos);
-
-//     }
-// });
-
-
-//  CÓDIGO DEL BARDO GUGLE
-document.addEventListener("DOMContentLoaded", function (){
-    data.products.forEach((item) => {
-      // Recuperar el identificador del producto
-      const idProducto = item.id;
+document.addEventListener('DOMContentLoaded', function () {
+  const productId = localStorage.getItem('ID_del_producto');
   
-      // Guardar el identificador del producto en el almacenamiento local
-      localStorage.setItem("productoSeleccionado", idProducto);
-  
-      // Mostrar la información del producto
-      mostrarEnContenedor();
-    });
-  });
-  
-  function mostrarEnContenedor(){
-      // Cambiar la URL de la solicitud HTTP
-      const urlRecuperacion = `https://japceibal.github.io/emercado-api/products/${localStorage.getItem("productoSeleccionado")}.json`;
-    
-      // Mostrar la información del producto
-      fetch(urlRecuperacion)
-      .then((response) => response.json())
-      .then((data) => {
-        mostrarInner(data.collection.items);
+  if (productId) {
+    // Realizar la solicitud para obtener los detalles del producto
+    fetch(`https://japceibal.github.io/emercado-api/products/${productId}.json`)
+      .then(response => response.json())
+      .then(product => {
+        // Construir el HTML para mostrar los detalles del producto
+        const detalleProductoHTML = `
+          <h1>${product.name}</h1>
+          <img src="${product.image}" alt="${product.name}">
+          <p>${product.description}</p>
+          <p>Precio: ${product.currency} ${product.cost}</p>
+          <p>Vendidos: ${product.soldCount}</p>
+          <!-- Agrega aquí cualquier otra información que desees mostrar -->
+        `;
+
+        // Mostrar los detalles del producto en la página
+        const detalleProductoContainer = document.getElementById('detalle-producto');
+        detalleProductoContainer.innerHTML = detalleProductoHTML;
       })
-      .catch((error) => {
-        console.error("Error al realizar la petición:", error);
-      });
-  };
-  
-  function mostrarInner(items) {
-      infoEnContenedor.innerHTML = "";
-
-      // Definir las constantes
-      const nombre = item.links[0].name;
-      const descripcion = item.data[0].description;
-      const costo = item.data[0].cost;
-      const moneda = item.data[0].currency;
-      const cantidadVendidos = item.data[0].soldCount;
-  
-      // Mostrar la información del producto
-      const itemsObtenidos = document.createElement("div");
-      itemsObtenidos.classList.add("items-resultantes");
-      itemsObtenidos.innerHTML = `
-          <h3>${nombre}</h3>
-          <p>${descripcion}</p>
-          <p>${moneda} Precio: ${costo}</p>
-          <p>Cantidad de vendidos: ${cantidadVendidos}</p>
-          <img src="${imageUrl}" alt="${title}">
-          `;
-  
-      infoEnContenedor.appendChild(itemsObtenidos);
-  };
-  
-  
+   
+  }
+});
