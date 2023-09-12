67e188fed55243ad6bfa5ea6d4364b8a43084e04
florencia menendez <flor_mm10@hotmail.com>
2023-09-12 16:19:43 -0300

    se muestran los comentarios por producto

--- js/product-info.js
+++ js/product-info.js
@@ -1,13 +1,13 @@
 /*ENTREGA Nª3 / CONSIGNA-2*/
 
-document.addEventListener('DOMContentLoaded', function () {
-  const productId = localStorage.getItem('ID_del_producto');
+document.addEventListener("DOMContentLoaded", function () {
+  const productId = localStorage.getItem("ID_del_producto");
 
   if (productId) {
     // Realizar la solicitud para obtener los detalles del producto
     fetch(`https://japceibal.github.io/emercado-api/products/${productId}.json`)
-      .then(response => response.json())
-      .then(product => {
+      .then((response) => response.json())
+      .then((product) => {
         // Construir el HTML para mostrar los detalles del producto
         const detalleProductoHTML = `
           <h1>${product.name}</h1>
@@ -17,7 +17,8 @@ document.addEventListener('DOMContentLoaded', function () {
           `;
 
         // Mostrar los detalles del producto en la página
-        const detalleProductoContainer = document.getElementById('detalle-producto');
+        const detalleProductoContainer =
+          document.getElementById("detalle-producto");
         detalleProductoContainer.innerHTML = detalleProductoHTML;
 
         // hacemos un bucle for-of para poder iterar el array de las imágenes correctamente
@@ -27,10 +28,62 @@ document.addEventListener('DOMContentLoaded', function () {
           img.alt = product.name;
 
           // mostrar las imágenes del producto en la página
-          const imagenProductoContainer = document.getElementById("detalle-producto");
+          const imagenProductoContainer =
+            document.getElementById("detalle-producto");
           imagenProductoContainer.appendChild(img);
         }
-      })
+        /*ENTREGA Nª3 / CONSIGNA-3*/
+        //crear los comentarios de los productos
+        //realizo la solicitud fetch
+        fetch(
+          `https://japceibal.github.io/emercado-api/products_comments/${productId}.json`
+        )
+          .then((response) => response.json())
+          .then((comments) => {
+            console.log(comments);
 
+            for (const comment of comments) {
+              const comentarioHTML = `
+          <div class="container-comentarios">
+              <div>
+                <div>
+                  <div class="comentarios-usuario">
+                    <div>
+                    <p class="nombre-usuario">${comment.user.toUpperCase()}</p>
+                    </div>
+                    <div>
+                    <p>${comment.dateTime}</p> 
+                    </div>
+                  </div>
+                  <span class="fa fa-star ${
+                    comment.score >= 1 && "checked"
+                  }"></span>
+                  <span class="fa fa-star ${
+                    comment.score >= 2 && "checked"
+                  }"></span>
+                  <span class="fa fa-star ${
+                    comment.score >= 3 && "checked"
+                  }"></span>
+                  <span class="fa fa-star ${
+                    comment.score >= 4 && "checked"
+                  }"></span>
+                  <span class="fa fa-star ${
+                    comment.score >= 5 && "checked"
+                  }"></span>
+                </div>
+                <div>
+                  <p>${comment.description}</p>
+                </div>
+              </div
+          </div>`;
+
+              const comentarios = document.getElementById(
+                "comentario-producto"
+              );
+
+              comentarios.innerHTML = comentarios.innerHTML + comentarioHTML;
+            }
+          });
+      });
   }
 });
