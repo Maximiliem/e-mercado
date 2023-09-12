8b7fe8fbbcca941c10cfb66f94721e06c2ec3d1a
Maximiliem <maximiliem@yahoo.com>
2023-09-11 22:47:14 -0300

    Add images to product-info.html
    Se agrega un bucle for-of para poder iterar por el array que contiene las imágenes de cada producto

--- js/product-info.js
+++ js/product-info.js
@@ -2,7 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   const productId = localStorage.getItem('ID_del_producto');
-  
+
   if (productId) {
     // Realizar la solicitud para obtener los detalles del producto
     fetch(`https://japceibal.github.io/emercado-api/products/${productId}.json`)
@@ -11,17 +11,26 @@ document.addEventListener('DOMContentLoaded', function () {
         // Construir el HTML para mostrar los detalles del producto
         const detalleProductoHTML = `
           <h1>${product.name}</h1>
-          <img src="${product.image}" alt="${product.name}">
           <p>${product.description}</p>
           <p>Precio: ${product.currency} ${product.cost}</p>
           <p>Vendidos: ${product.soldCount}</p>
-          <!-- Agrega aquí cualquier otra información que desees mostrar -->
-        `;
+          `;
 
         // Mostrar los detalles del producto en la página
         const detalleProductoContainer = document.getElementById('detalle-producto');
         detalleProductoContainer.innerHTML = detalleProductoHTML;
+
+        // hacemos un bucle for-of para poder iterar el array de las imágenes correctamente
+        for (const image of product.images) {
+          const img = document.createElement("img");
+          img.src = image;
+          img.alt = product.name;
+
+          // mostrar las imágenes del producto en la página
+          const imagenProductoContainer = document.getElementById("detalle-producto");
+          imagenProductoContainer.appendChild(img);
+        }
       })
-   
+
   }
 });
