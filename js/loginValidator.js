de9741806e8215906775b43aad0e06f80e517483
Maximiliem <maximiliem@yahoo.com>
2023-09-25 21:33:36 -0300

    create endSession function

--- js/loginValidator.js
+++ js/loginValidator.js
@@ -10,97 +10,100 @@ document.addEventListener('DOMContentLoaded', () => {
     // }
     const elementoMostrarCorreo = document.createElement('span');
     elementoMostrarCorreo.id = "correoMensajeLogueado"
-// Función inicio de sesión
-function login() {
-    // Obtiene los valores del correo electrónico y la contraseña
-    const userEmail = document.getElementById("useremail").value;
-    const userPassword = document.getElementById("userpassword").value;
-  
-    // Verificar si el correo electrónico y la contraseña no están vacíos
-    if (userEmail !== "" && userPassword !== "") {
-      // Almacena el estado de inicio de sesión en el almacenamiento local
-      localStorage.setItem("logueado", "true");
-      // Guardamos el correo ingresado con la clave "email"
-      localStorage.setItem("email", userEmail);
-  
-      // Actualiza la etiqueta del botón de inicio de sesión para que muestre cerrar sesión
-      const longinbuttonlabel = document.getElementById("longinbuttonlabel");
-      longinbuttonlabel.textContent = "Cerrar sesión";
-  
-      // Oculta la etiqueta de crear cuenta
-      const botonCrearCuenta = document.getElementById("botonCrearCuenta");
-      const barranav = document.getElementById("navbarNav");
-      // Ocultar el botón cambiando su estilo
-      botonCrearCuenta.style.display = "none";
-  
-      // Oculta el formulario de iniciar sesión
-      const containerPopup = document.querySelector('.container-popup');
-      containerPopup.style.display = 'none';
-  
-      // Crear un elemento que muestre un saludo al usuario (Correo Loggeado)
-      
-      barranav.appendChild(elementoMostrarCorreo);
-      // Modificamos la classe para mostrarlo y traemos el valor del locar storage
-      elementoMostrarCorreo.className = 'nav-link text-white';
-      elementoMostrarCorreo.textContent = 'Bienvenido: ' + localStorage.getItem('email');
+    // Función inicio de sesión
+    function login() {
+        // Obtiene los valores del correo electrónico y la contraseña
+        const userEmail = document.getElementById("useremail").value;
+        const userPassword = document.getElementById("userpassword").value;
+
+        // Verificar si el correo electrónico y la contraseña no están vacíos
+        if (userEmail !== "" && userPassword !== "") {
+            // Almacena el estado de inicio de sesión en el almacenamiento local
+            localStorage.setItem("logueado", "true");
+            // Guardamos el correo ingresado con la clave "email"
+            localStorage.setItem("email", userEmail);
+
+            // Actualiza la etiqueta del botón de inicio de sesión para que muestre cerrar sesión
+            const longinbuttonlabel = document.getElementById("longinbuttonlabel");
+            longinbuttonlabel.textContent = "Cerrar sesión";
+
+            // Oculta la etiqueta de crear cuenta
+            const botonCrearCuenta = document.getElementById("botonCrearCuenta");
+            const barranav = document.getElementById("navbarNav");
+            // Ocultar el botón cambiando su estilo
+            botonCrearCuenta.style.display = "none";
+
+            // Oculta el formulario de iniciar sesión
+            const containerPopup = document.querySelector('.container-popup');
+            containerPopup.style.display = 'none';
+
+            // Crear un elemento que muestre un saludo al usuario (Correo Loggeado)
+
+            barranav.appendChild(elementoMostrarCorreo);
+            // Modificamos la classe para mostrarlo y traemos el valor del locar storage
+            elementoMostrarCorreo.className = 'nav-link text-white';
+            elementoMostrarCorreo.textContent = 'Bienvenido: ' + localStorage.getItem('email');
+        }
     }
-  }
-  
-  // Función para el cierre de sesión
-  function logout() {
-    // Elimina el estado de inicio de sesión del almacenamiento local
-    localStorage.removeItem("logueado");
-    localStorage.removeItem("email");
-  
-    // Actualiza la etiqueta del botón por inicio de sesión
-    const longinbuttonlabel = document.getElementById("longinbuttonlabel");
-    longinbuttonlabel.textContent = "Iniciar sesión";
-  
-    // Muestra nuevamente el formulario de inicio de sesión
-    const containerPopup = document.querySelector('.container-popup');
-    containerPopup.style.display = 'block';
-  
-    // Muestra nuevamente el botón de crear usuario 
-    const botonCrearCuenta = document.getElementById("botonCrearCuenta");
 
-    // Ocultar el botón cambiando su estilo
-    botonCrearCuenta.style.display = "block";
+    // Función para el cierre de sesión
+    function logout() {
+        // Elimina el estado de inicio de sesión del almacenamiento local
+        localStorage.removeItem("logueado");
+        localStorage.removeItem("email");
 
-    // Ocultar el mensaje Cambiando su estio 
-    const elementoMostrarCorreo = document.getElementById("correoMensajeLogueado")
-    //elementoMostrarCorreo.style,display = 'none';
-    elementoMostrarCorreo.parentNode.removeChild(elementoMostrarCorreo)
+        // Actualiza la etiqueta del botón por inicio de sesión
+        const longinbuttonlabel = document.getElementById("longinbuttonlabel");
+        longinbuttonlabel.textContent = "Iniciar sesión";
 
-  }
-  
-    // Se crea una función que almacena la constante ya existente que generaba el login
+        // Muestra nuevamente el formulario de inicio de sesión
+        const containerPopup = document.querySelector('.container-popup');
+        containerPopup.style.display = 'block';
+
+        // Muestra nuevamente el botón de crear usuario 
+        const botonCrearCuenta = document.getElementById("botonCrearCuenta");
+
+        // Ocultar el botón cambiando su estilo
+        botonCrearCuenta.style.display = "block";
+
+        // Ocultar el mensaje Cambiando su estio 
+        const elementoMostrarCorreo = document.getElementById("correoMensajeLogueado")
+        //elementoMostrarCorreo.style,display = 'none';
+        elementoMostrarCorreo.parentNode.removeChild(elementoMostrarCorreo)
+
+    }
+
+    // Se crea una función que almacena la constante ya existente que generaba el login y se envita el doble loggeo
     // Agregar evento click al botón loginbutton
-    function logbtn(){
+    function logbtn() {
         const loginbutton = document.getElementById("loginbutton");
-    if (loginbutton) {
-        loginbutton.addEventListener('click', () => {
-            // Verifica si el usuario ya está logueado
-            if (localStorage.getItem("logueado")) {
-                // Realiza el proceso de cierre de sesión
-                logout();
-            } else {
-                login();
-            }
-        });
-    }
+        if (loginbutton) {
+            loginbutton.addEventListener('click', () => {
+                // Verifica si el usuario ya está logueado
+                if (localStorage.getItem("logueado")) {
+                    // Realiza el proceso de cierre de sesión
+                    logout();
+                } else {
+                    login();
+                }
+            });
+        }
     };
     logbtn();
-    
 
 
-    // Agregar evento click al botón longinbuttonlabel (que ahora dice "Cerrar sesión")
-    const longinbuttonlabel = document.getElementById("longinbuttonlabel");
-    if (longinbuttonlabel) {
-        longinbuttonlabel.addEventListener('click', () => {
-            // Realiza el proceso de cierre de sesión al hacer clic en "Cerrar sesión"
-            logout();
-        });
-    }
+    function endSession() {
+        // Agregar evento click al botón longinbuttonlabel (que ahora dice "Cerrar sesión")
+        const longinbuttonlabel = document.getElementById("longinbuttonlabel");
+        if (longinbuttonlabel) {
+            longinbuttonlabel.addEventListener('click', () => {
+                // Realiza el proceso de cierre de sesión al hacer clic en "Cerrar sesión"
+                logout();
+            });
+        }
+    };
+    endSession();
+
 
     // Verificar y actualizar la etiqueta del botón cuando la página carga
     // const userlogueado = localStorage.getItem("logueado");
