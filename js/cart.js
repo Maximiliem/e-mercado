ff6287d45a47acc2248180c55c9415098602baf4
mpenen52 <mpenen52@gmail.com>
2023-10-07 23:09:45 -0300

    Insertar Fila
    Inserta solo la fila y no la tabla completa, a futuro sirve para que no inserte los nombres de la tabla por cada producto que quiera comprar.

--- js/cart.js
+++ js/cart.js
@@ -1,60 +1,47 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    const cartUrl = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
+document.addEventListener("DOMContentLoaded", () => {
+  const cartUrl = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
 
- // Realizar la solicitud Fetch para obtener el carrito de compras
-fetch(cartUrl)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Error al obtener el carrito de compras.");
-    }
-    return response.json();
-  })
-  .then((data) => {
+  // Realizar la solicitud Fetch para obtener el carrito de compras
+  fetch(cartUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error al obtener el carrito de compras.");
+      }
+      return response.json();
+    })
+    .then((data) => {
 
-    const user = data.user;
-    const articles = data.articles;
+      const user = data.user;
+      const articles = data.articles;
 
-    if (articles.length > 0) {
+      if (articles.length > 0) {
 
-      const carritoElement = document.getElementById("Articulos");
+        const carritoElement = document.getElementById("Articulos");
+        const tableBody = carritoElement.querySelector('tbody');
 
-      articles.forEach((producto) => {
-        const productoHtml = `
-        <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nombre</th>
-            <th scope="col">Costo</th>
-            <th scope="col">Cantidad</th>
-            <th scope="col">Moneda</th>
-            <th scope="col">Subtotal</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th> <img width="50"src="${producto.image}" alt="Imagen del producto"> </th>
-            <th> ${producto.name}</th>
-            <th> ${producto.unitCost}</th>
-            <th> <input type="text" id="cantidadInput_${producto.id}" value="${producto.count}" style="width: 50px; text-align: center"></th>
-            <th> ${producto.currency}</th>
-            <th> ${producto.unitCost * producto.count}</th>
-          </tr>
-        </tbody>
-        </table>
+        articles.forEach((producto) => {
+          const row = document.createElement('tr');
+
+          row.innerHTML = `
+            <th scope="row"><img width="50" src="${producto.image}" alt="Imagen del producto"></th>
+            <td>${producto.name}</td>
+            <td>${producto.unitCost}</td>
+            <td><input type="text" id="cantidadInput_${producto.id}" value="${producto.count}" style="width: 50px; text-align: center"></td>
+            <td>${producto.currency}</td>
+            <td>${producto.unitCost * producto.count}</td>
           `;
-        carritoElement.innerHTML += productoHtml;
-      });
 
-      console.log(articles);
+          tableBody.appendChild(row);
+        });
 
-    } else {
-      console.error("El carrito de compras está vacío.");
-    }
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+        console.log(articles);
+
+      } else {
+        console.error("El carrito de compras está vacío.");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
 })
-
